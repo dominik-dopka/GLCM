b1fcfcf52038f523b7bf63075bea39aeecb9df1c
dominik-dopka <dominik.dopka@gmail.com>
2021-01-19 22:31:49 +0100

    Wybór obrazków

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,92 @@ namespace GLCM
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.chooseImagesButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.nextImageButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.previousImageButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chooseImagesButton
+            // 
+            this.chooseImagesButton.Location = new System.Drawing.Point(27, 48);
+            this.chooseImagesButton.Name = "chooseImagesButton";
+            this.chooseImagesButton.Size = new System.Drawing.Size(94, 23);
+            this.chooseImagesButton.TabIndex = 0;
+            this.chooseImagesButton.Text = "Choose Images";
+            this.chooseImagesButton.UseVisualStyleBackColor = true;
+            this.chooseImagesButton.Click += new System.EventHandler(this.chooseImagesButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(168, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(259, 253);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // nextImageButton
+            // 
+            this.nextImageButton.Location = new System.Drawing.Point(352, 292);
+            this.nextImageButton.Name = "nextImageButton";
+            this.nextImageButton.Size = new System.Drawing.Size(75, 23);
+            this.nextImageButton.TabIndex = 2;
+            this.nextImageButton.Text = "Next";
+            this.nextImageButton.UseVisualStyleBackColor = true;
+            this.nextImageButton.Click += new System.EventHandler(this.nextImageButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // previousImageButton
+            // 
+            this.previousImageButton.Location = new System.Drawing.Point(168, 292);
+            this.previousImageButton.Name = "previousImageButton";
+            this.previousImageButton.Size = new System.Drawing.Size(75, 23);
+            this.previousImageButton.TabIndex = 3;
+            this.previousImageButton.Text = "Previous";
+            this.previousImageButton.UseVisualStyleBackColor = true;
+            this.previousImageButton.Click += new System.EventHandler(this.previousImageButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "0 images chosen";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.previousImageButton);
+            this.Controls.Add(this.nextImageButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.chooseImagesButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button chooseImagesButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button nextImageButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button previousImageButton;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,75 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace GLCM
 {
     public partial class Form1 : Form
     {
+
+        private List<String> imagePaths;
+        private int imageIndex;
+
         public Form1()
         {
             InitializeComponent();
+
+            pictureBox1.BackColor = Color.Black;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            nextImageButton.Enabled = false;
+            previousImageButton.Enabled = false;
+        }
+
+        private void chooseImagesButton_Click(object sender, EventArgs e)
+        {
+            string message = "";
+
+            openFileDialog1.Multiselect = true;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                imagePaths = new List<String>();
+                imageIndex = 0;
+
+                foreach (string file in openFileDialog1.FileNames)
+                {
+                    //imagePaths.Add(Path.GetFileName(file));
+                    imagePaths.Add(file);
+                    //message += Path.GetFileName(file) + " - " + file + Environment.NewLine;
+                }
+
+                foreach(string path in imagePaths)
+                {
+                    message += path + Environment.NewLine;
+                }
+                MessageBox.Show(message);
+                pictureBox1.ImageLocation = imagePaths.First();
+
+                if (imagePaths.Count > 1)
+                    nextImageButton.Enabled = true;
+            }
+        }
+
+        private void nextImageButton_Click(object sender, EventArgs e)
+        {
+            imageIndex += 1;
+            pictureBox1.ImageLocation = imagePaths[imageIndex];
+
+            if (imageIndex == imagePaths.Count - 1)
+                nextImageButton.Enabled = false;
+
+            previousImageButton.Enabled = true;
+        }
+
+        private void previousImageButton_Click(object sender, EventArgs e)
+        {
+            imageIndex -= 1;
+            pictureBox1.ImageLocation = imagePaths[imageIndex];
+
+            if (imageIndex == 0)
+                previousImageButton.Enabled = false;
+
+            nextImageButton.Enabled = true;
         }
     }
 }
